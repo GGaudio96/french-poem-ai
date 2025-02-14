a530848f68f3af3a17b04ae92e3cd62e928b5c05
GGaudio96 <GGaudio96@users.noreply.github.com>
2025-02-14 18:06:16 +0100

    added api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-function generatePoem(event) {
-    event.preventDefault();
+function displayPoem(response) {
 
-    
-
-new Typewriter("#poem", {
-  strings: ["Il n'y a pas d'amour heureux"],
+  new Typewriter("#poem", {
+  strings: response.data.answer,
   autoStart: true,
 });
    
 }
+
+
+function generatePoem(event) {
+    event.preventDefault();
+let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "35b4b416c408f4a70ee442o1td5f0b38";
+    let prompt = `generate a french poem about ${instructionsInput.value}`;
+    let context = "generate a short one line romantic poem following the user instructions in the french language";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayPoem);
+    
+}
+
 
 
 let poemFormElement = document.querySelector("#poem-generator");
